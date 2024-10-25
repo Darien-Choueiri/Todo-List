a114098c5454fd7ea0f4d0217062167d8064c5f0
Darien Choueiri <darien.choueiri@gmail.com>
2024-10-25 17:56:30 -0400

    Add popup form for adding tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import './style.css';
 
+/* 
+const addTaskForm = document.querySelector(".addTaskForm");
+const addTaskButton = document.querySelector(".addTask-button");
+const closeAddTask = document.querySelector(".exitAddTask");
+
+function popupShow() {
+    addTaskForm.classList.add("show");
+}
+
+function popupExit() {
+    addTaskForm.classList.remove("show");
+}
+addTaskButton.addEventListener('click', () => {
+    popupShow();
+});
+
+closeAddTask.addEventListener('click', ()=> {
+    popupExit();
+});
+ */
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".addTask-button");
+const closeButton = document.querySelector(".exitAddTask");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
