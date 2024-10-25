c07e371923d2a27cbfdf8ef8813594d50a03613c
Darien Choueiri <darien.choueiri@gmail.com>
2024-10-25 17:57:57 -0400

    Remove redundant code for dialog manipulation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import './style.css';
 
-/* 
-const addTaskForm = document.querySelector(".addTaskForm");
-const addTaskButton = document.querySelector(".addTask-button");
-const closeAddTask = document.querySelector(".exitAddTask");
-
-function popupShow() {
-    addTaskForm.classList.add("show");
-}
-
-function popupExit() {
-    addTaskForm.classList.remove("show");
-}
-addTaskButton.addEventListener('click', () => {
-    popupShow();
-});
-
-closeAddTask.addEventListener('click', ()=> {
-    popupExit();
-});
- */
-
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".addTask-button");
 const closeButton = document.querySelector(".exitAddTask");
